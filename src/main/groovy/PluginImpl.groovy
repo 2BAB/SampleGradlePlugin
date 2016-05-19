48a97b89f3615c3c59c9a5efb73c20658180ea6f
2BAB <xx2bab@gmail.com>
2016-05-19 19:08:07 +0800

    init

---
+++ src/main/groovy/PluginImpl.groovy
@@ -0,0 +1,13 @@
+package PackageName.ProjectName
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import com.android.build.gradle.AppPlugin
+
+public class PluginImpl implements Plugin<Project> {
+    void apply(Project project) {
+        project.task('testTask') << {
+            println "Hello gradle plugin"
+        }
+    }
+}
